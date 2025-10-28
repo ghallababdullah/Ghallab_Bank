4ed2bd532babcba618e9e1aa88a92eaa7b5081db
Abdullah Ghallab <113211590+ghallababdullah@users.noreply.github.com>
2025-10-29 00:56:40 +0300

    Built the initial program

---
+++ src/main/java/com/ghallab/Ghallab_Bank/GhallabBankApplication.java
@@ -0,0 +1,13 @@
+package com.ghallab.Ghallab_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhallabBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhallabBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghallab/Ghallab_Bank/GhallabBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghallab.Ghallab_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhallabBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
